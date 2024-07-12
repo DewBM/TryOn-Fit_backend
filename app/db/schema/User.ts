a14c2898fe7f86d948349afae0698c313f779d3d
DewBM <dewmitha.mahiti@gmail.com>
2024-07-12 18:11:05 +0530

    add userId for User table.

--- app/db/schema/User.ts
+++ app/db/schema/User.ts
@@ -1,7 +1,8 @@
-import { pgTable } from "drizzle-orm/pg-core";
+import { integer, numeric, pgTable, serial } from "drizzle-orm/pg-core";
 import { text } from 'drizzle-orm/pg-core';
 
 export const users = pgTable('User', {
+   userId: serial('userId'),
    username: text('username').primaryKey(),
    password: text('password').notNull(),
 });
