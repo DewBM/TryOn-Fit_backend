ae7e7f8af737ac4d8a6f32f94a49e3c6f078a371
DewBM <dewmitha.mahiti@gmail.com>
2024-07-13 17:11:52 +0530

    add roles to users.

--- app/db/schema/User.ts
+++ app/db/schema/User.ts
@@ -1,8 +1,19 @@
+import { relations } from "drizzle-orm";
 import { integer, numeric, pgTable, serial } from "drizzle-orm/pg-core";
 import { text } from 'drizzle-orm/pg-core';
+import { roles } from "./Role";
 
 export const users = pgTable('User', {
    userId: serial('userId'),
    username: text('username').primaryKey(),
    password: text('password').notNull(),
+   role: text('role').references(() => roles.role)
 });
+
+export const userRelations = relations(users, ({ one }) => ({
+   role: one(roles, {
+      fields: [users.role],
+      references: [roles.role]
+   })
+}));
+
