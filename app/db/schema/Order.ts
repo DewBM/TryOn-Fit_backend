f86a45854bb292bb972706136da00872977d6a36
DewBM <dewmitha.mahiti@gmail.com>
2024-07-30 11:05:08 +0530

    create order tables schemas.

---
+++ app/db/schema/Order.ts
@@ -0,0 +1,25 @@
+import { date, integer, numeric, pgEnum, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+import { customersTable } from "./Customer";
+import { productVariantsTable } from "./Product";
+
+const orderStatusEnum = pgEnum('order_status', ['']);
+
+export const ordersTable = pgTable('order', {
+   order_id: serial('order_id').primaryKey(),
+   customer_id: text('customer_id').references(()=> customersTable.customer_id),
+   order_status: orderStatusEnum('order_status'),
+   order_date: timestamp('order_date'),
+   delivery_date: timestamp('delivery_date'),
+   delivery_address: text('delivery_address'),
+   sub_total: numeric('sub_total'),
+   discount: numeric('discount'),
+});
+
+
+export const orderItemsTable = pgTable('order_item', {
+   order_id: integer('order_id').references(()=> ordersTable.order_id),
+   item_id: text('item_id').references(()=> productVariantsTable.variant_id),
+   quantity: integer('quantity'),
+   price: numeric('price'),
+   disount: numeric('discount'),
+});
--- app/db/schema/index.ts
+++ app/db/schema/index.ts
@@ -1,9 +1,22 @@
-import { customer } from "./Customer";
+import { customersTable } from "./Customer";
 import { productsTable, sizeChartsTable } from "./Product";
 import { employeesTable } from "./Employee";
 import { users } from "./User";
 import { suppliersTable } from "./Supplier";
 import { inventoriesTable } from "./Inventory";
 import { cartItemsTable, cartsTable } from "./Cart";
+import { orderItemsTable, ordersTable } from "./Order";
 
-export {customer, productsTable, users, employeesTable, suppliersTable, sizeChartsTable, inventoriesTable, cartsTable, cartItemsTable }
+export {
+   customersTable,
+   productsTable,
+   users,
+   employeesTable,
+   suppliersTable,
+   sizeChartsTable,
+   inventoriesTable,
+   cartsTable,
+   cartItemsTable,
+   ordersTable,
+   orderItemsTable
+}
