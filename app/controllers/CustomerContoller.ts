7ccf92d7ac6ed25fc6420c606563eff3746eb98d
Himasha Senarathne <123543567+Himasha-2001@users.noreply.github.com>
2024-09-01 18:29:56 +0530

    Update CustomerContoller.ts

--- app/controllers/CustomerContoller.ts
+++ app/controllers/CustomerContoller.ts
@@ -3,15 +3,11 @@ import { getCustomerByCustomerId } from "../services/CustomerService";
 
 export async function doGet(req: Request, res: Response) {
   const customer_id = req.query.customer_id as string;
-
   if (customer_id) {
-    try {
       const result = await getCustomerByCustomerId(parseInt(customer_id, 10));
-      res.status(200).json(result);
-    } catch (error) {
-      res.status(500).json({ error: "Could not get customer data" });
-    }
+    if(result.isSuccess){
+      res.status(200).json(result);  
   } else {
-    res.status(400).json({ error: "Customer ID is required." });
+    res.status(500).json({error: "Could not get customer data" });
   }
 }
