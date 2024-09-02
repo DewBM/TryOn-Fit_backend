e9d51193d530c9fc983be3f95a884df142b9be7d
pehan <pehnrathnayake159@gmai.com>
2024-09-02 20:49:08 +0530

    fix errors

--- app/services/SupplierService.ts
+++ app/services/SupplierService.ts
@@ -1,14 +1,14 @@
-import { getURL } from "next/dist/shared/lib/utils";
+// import { getURL } from "next/dist/shared/lib/utils";
 import {createNewSupplier,getAllSupplier, updateSupplierData,deleteExistSuplier} from "../db/dao/SupplierDao"
 
-interface supDataType {
-    supplier_id: string,
-   first_name: string,
-   last_name: string,
-   brand_name: string,
-   contact_no: string,
-   address: string
-}
+// interface supDataType {
+//     supplier_id: string,
+//    first_name: string,
+//    last_name: string,
+//    brand_name: string,
+//    contact_no: string,
+//    address: string
+// }
 
 export const createSupplier = (supData :supDataType) => {
    return createNewSupplier(supData);
@@ -18,7 +18,7 @@ export const getSupplier = () => {
    return getAllSupplier()
 }
 
-export const updateSupplier = (supData : supDataType, id : string) => {
+export const updateSupplier = (supData : SelectSupllier, id : string) => {
    return updateSupplierData(supData, id);
 }
 export const deleteSupplier = (id : {supplier_id:string}) => {
