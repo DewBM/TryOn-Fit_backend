b5215fc12b0b51041e794712e367efbf97bf7151
Himasha-2001 <2021is085@stu.ucsc.cmb.ac.lk>
2024-09-02 20:03:17 +0530

    Fix typo

--- app/controllers/CustomerContoller.ts
+++ app/controllers/CustomerContoller.ts
@@ -4,14 +4,16 @@ import { getCustomerByCustomerId } from "../services/CustomerService";
 export async function doGet(req: Request, res: Response) {
   const customer_id = req.query.customer_id as string;
   if (customer_id) {
-      const result = await getCustomerByCustomerId(parseInt(customer_id, 10));
-    if(result.isSuccess){
-      res.status(200).json(result);  
-  } else {
+    const result = await getCustomerByCustomerId(parseInt(customer_id, 10));
+    if (result.isSuccess) {
+      res.status(200).json(result);
+    } else {
       res.status(500).json({
-      isSuccess:false,
-      data:null,
-      msg: "Could not get customer data",
-      error:" " });
+        isSuccess: false,
+        data: null,
+        msg: "Could not get customer data",
+        error: " ",
+      });
+    }
   }
 }
