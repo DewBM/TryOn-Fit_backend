298989bcfbe6823e533a5146772f307268545f52
sapunethmini <sapuninethmini888@gmail.com>
2024-11-26 22:57:57 +0530

    did some changes in bodymeasuremnt part

--- app/db/dao/customerDAO.ts
+++ app/db/dao/customerDAO.ts
@@ -27,6 +27,7 @@ export async function getCustomerByCustomerId(customer_id: number) {
 }
 
 
+//body measrumenets 
 export async function addBodyMeasurements(customer_id: number, measurements: Record<string, number>) {
   try {
     await db.insert(bodyMeasurementsTable).values({
