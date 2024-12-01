38c7ef7f2f5a1d9b24dee45f14d0c32383407a87
SapnaNethmini <sapnanethmini98@gmail.com>
2024-12-02 03:24:09 +0530

    Inquiry report table updated

--- app/db/schema/InquiryForm.ts
+++ app/db/schema/InquiryForm.ts
@@ -1,17 +1,20 @@
 import { pgTable, serial, text ,integer, numeric} from "drizzle-orm/pg-core";
 import { ordersTable } from "./Order";
 import { productsTable } from "./Product";
+import { customersTable} from "./Customer";
 
 export const inquiry_reportTable = pgTable('InquiryReport', {
 
   inquiry_id: serial('inquiry_id').primaryKey(),
   order_id: integer('order_id').references(()=> ordersTable.order_id),
   product_id: text('product_id').references(()=>productsTable.product_id),
+  customer_id: integer('customer_id').references(()=>customersTable.customer_id),
   issue_type: text('issue_type').notNull(),
   image: text('image'),
   issue_description: text('issue_description').notNull(),
   additional_comments: text('additional_comments'),
   solution : text('solution'),
+  name : text('name'),
  
 
 
